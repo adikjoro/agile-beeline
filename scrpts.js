7472faf98ec6f57b545aff3d5e61b8d91a113b76
Adikjoro <adikjoro@gmail.com>
2018-06-27 19:47:19 +0600

    add icons

---
+++ scrpts.js
@@ -0,0 +1,196 @@
+const data = {
+    handbooks: [
+        {
+            title: "Scrum Революционный метод управления проектами. Автор Джефф Сазерленд",
+            desc: "275 стр",
+            href: "https://eurasia.veon.com/docs/DOC-9404-scrumpdf"
+        },
+        {
+            title: "Scrum Guide. Автор Джефф Сазерлен и Кен Швабер",
+            desc: "26 стр",
+            href: "https://eurasia.veon.com/docs/DOC-13508-2017-scrum-guide-russianpdf"
+        },
+        {
+            title: "Agile Manifesto",
+            desc: "1 стр",
+            href: "http://agilemanifesto.org/iso/ru/manifesto.html"
+        },
+        {
+            title: "Agile Principles",
+            desc: "1 стр",
+            href: "http://agilemanifesto.org/iso/ru/principles.html"
+        },
+        {
+            title: "Глоссарий по agile/scrum",
+            desc: "5 стр",
+            href: "https://www.scrumstudy.com/freeresources/scrum-agile-glossary"
+        }
+    ],
+    online: [
+        {
+            title: "Предпосылки появления agile. Курс читает Дмитрий Павлов ",
+            desc: "2 часа 20 минут",
+            href: "https://drive.google.com/open?id=14J0SLLM2maX9_p7uqPq8aM24IbRSruEx"
+        },
+        {
+            title: "Agile Requirements Foundations. Angela Wick.",
+            desc: "1 час 43 минуты",
+            href: "https://www.linkedin.com/learning/agile-requirements-foundations/welcome?u=2195290"
+        },
+        {
+            title: "Learn scrum online.",
+            desc: "",
+            href: "https://edu.vivifyscrum.com/chapters"
+        },
+        { title: "Agile для новичков", desc: "", href: "http://www.agilebasics.ru/" },
+        {
+            title: "Основы Agile: метод Scrum. Курс читает Асхат Уразбаев.",
+            desc: "2 часа 10 минут",
+            href: "https://webtutor.beeline.kz/view_doc.html?mode=course&doc_id=6429184362312526909&object_id=6436948958405156840"
+        },
+        {
+            title:
+                "Agile & Scrum – знакомство и легкое погружение. Курс читает Артем Быковец",
+            desc: "1 час 37 минут",
+            href: "https://www.youtube.com/watch?v=2uFA3f74D0Q&t=353s"
+        },
+        {
+            title:
+                "Transitioning from Waterfall to Agile Project Management. Kelley O'Connell.",
+            desc: "1 час 32 минут",
+            href: "https://www.linkedin.com/learning/transitioning-from-waterfall-to-agile-project-management?u=2195290"
+        },
+        {
+            title: "Scrum Basic. Kelley O'Connell",
+            desc: "1 час 2 минуты",
+            href: "https://www.linkedin.com/learning/scrum-the-basics"
+        },
+        {
+            title: "Agile Project Management Principles. Bob McGannon",
+            desc: "1 час 16 минуты",
+            href: "https://www.linkedin.com/learning/agile-project-management-principles?u=2195290"
+        },
+        {
+            title: "Free online Scrum Training Series",
+            desc: "1 час 20 минут",
+            href: "http://scrumtrainingseries.com/"
+        },
+        {
+            title: "Introduction to Agile - Transformation, Best Practices and Common Problems. Martin DuPont.",
+            desc: "1 час 29 минут",
+            href: "https://www.youtube.com/watch?v=fCE1PmtbGXQ"
+        },
+        {
+            title: "30 Minutes Free Agile Scrum Training",
+            desc: "30 минут",
+            href:
+                "https://masterofproject.com/p/agile-scrum-overview-agile-scrum-certification-info"
+        },
+        {
+            title: "Мультик - what is Agile? Марк Шед",
+            desc: "12 минут",
+            href: "https://www.youtube.com/watch?v=Z9QbYZh1YXY"
+        },
+        {
+            title: "Мультик - what is Scrum? Стив Стедман",
+            desc: "8 минут",
+            href: "https://www.youtube.com/watch?v=9TycLR0TqFA&t=4s"
+        },
+        {
+            title: "Гибкое управление продуктом в двух словах",
+            desc: "16 минут",
+            href: "https://www.youtube.com/watch?v=mIVRFYjIZ5A&feature=youtu.be"
+        },
+        {
+            title: "Agile Product Ownership in a Nutshell",
+            desc: "16 минут",
+            href: "https://www.youtube.com/watch?v=502ILHjX9EE&feature=youtu.be"
+        },
+        {
+            title: "Scrum и Kanban: Выжимаем максимум",
+            desc: "78 стр",
+            href: "https://eurasia.veon.com/docs/DOC-9403-scrumandkanbanrufinalpdf"
+        },
+        {
+            title: "Introduction to Scrum",
+            desc: "8 минут",
+            href: "https://www.youtube.com/watch?v=9TycLR0TqFA&feature=youtu.be"
+        },
+        {
+            title: "What is a 'Scrum Master'? - Scrum Guide",
+            desc: "4 минуты",
+            href:
+                "https://www.youtube.com/watch?v=QbPkcfzi2HI&index=1&list=PLpiOR7CBNvlqPjsa3vsB6BhhDDeUJOeyZ"
+        },
+        {
+            title: "Олег Бахмутов, Михаил Плотников, Илья Емельянов. Три кита Agile",
+            desc: "1 час 5 минут",
+            href: "https://www.youtube.com/watch?v=0PLBnPZNZN4&feature=youtu.be"
+        }
+    ],
+    presents: [
+        {
+            href: "https://www.atlassian.com/agile",
+            title: "What is Agile?",
+            desc: "5 стр"
+        },
+
+        {
+            href: "http://www.myshared.ru/slide/548116/",
+            title: "Презентация agile/scrum.",
+            desc: "22 слайда"
+        },
+
+        {
+            href: "https://www.scrumstudy.com/pdf/Scrumstudy%20Process%20Chart-2-%2018-10-2012.pdf",
+            title: "Презентация. Scrum one pager.",
+            desc: "2 слайда"
+        },
+
+        {
+            href: "https://kaktus.media/doc/362616_nikolay_borisov:_kak_delat_za_2_nedeli_to_chto_drygie_yspevaut_za_polgoda.html",
+            title: "Интервью с Николаем Борисовым.",
+            desc: "1 стр"
+        },
+
+        {
+            href: "https://www.atlassian.com/agile/scrum",
+            title: "What is Scrum? Dan Radigan",
+            desc: "5 стр"
+        },
+
+        {
+            href: "https://eurasia.veon.com/community/eurasia/kg/journey-2017/journey-2017-archive/future-om/agile-sky-journey/blog/2017/11/18/agile-%D0%BF%D0%BE%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B8%D1%82%D0%BE%D0%B3%D0%BE%D0%B2-agile-kyrgyzstan-meetup1",
+            title: "[AGILE] Подведение итогов Agile Kyrgyzstan Meetup#1",
+            desc: ""
+        },
+
+        {
+            href: "https://www.forbes.com/sites/stevedenning/2016/11/26/can-big-organizations-be-agile/#e8c0e7b38e79",
+            title: " Can Big Organizations Be Agile? Steve Denning",
+            desc: ""
+        },
+
+        {
+            href: "https://www.forbes.com/sites/stevedenning/2015/10/27/surprise-microsoft-is-agile/#603c0e6c2867",
+            title: "Surprise: Microsoft Is Agile. Steve Denning.",
+            desc: ""
+        },
+
+        {
+            href: "https://www.forbes.com/sites/stevedenning/2015/10/29/microsofts-sixteen-keys-to-becoming-agile-at-scale/#cfc0ec515ce7",
+            title: "Microsoft's 16 Keys To Being Agile At Scale. Steve Denning,",
+            desc: ""
+        }
+    ]
+}
+$("document").ready(function() {
+    $(".education__item").foreach(function() {
+        dataType = data[$(this).attr("data-title")]
+        $(this).click(function() {
+            for(const p in dataType) {
+                
+            }
+        })
+    })
+});
